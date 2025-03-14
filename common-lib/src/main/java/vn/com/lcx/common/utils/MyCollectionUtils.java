f8a6d2b08fe75dbdaebcc2bcbae31c90528619be
locvt1 <locvt1@hdbank.com.vn>
2025-03-14 14:41:50 +0700

    add utils method to split list

---
+++ common-lib/src/main/java/vn/com/lcx/common/utils/MyCollectionUtils.java
@@ -0,0 +1,34 @@
+package vn.com.lcx.common.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MyCollectionUtils {
+
+    public static <T> List<List<T>> splitListIntoBatches(List<T> inputList, int maxBatchSize, int maxBatches) {
+        if (maxBatchSize == 0) {
+            maxBatchSize = 50_000;
+        }
+        if (maxBatches == 0) {
+            maxBatches = 8;
+        }
+        int totalSize = inputList.size();
+
+        // Calculate actual batch count, ensuring it does not exceed maxBatches
+        int batchCount = Math.min((int) Math.ceil((double) totalSize / maxBatchSize), maxBatches);
+        List<List<T>> batches = new ArrayList<>(batchCount);
+
+        for (int i = 0; i < batchCount; i++) {
+            int fromIndex = i * maxBatchSize;
+            int toIndex = Math.min(fromIndex + maxBatchSize, totalSize);
+            batches.add(new ArrayList<>(inputList.subList(fromIndex, toIndex)));
+        }
+
+        return batches;
+    }
+
+}
