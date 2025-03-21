bb706bdb870a5c70d27a696dee0a77d1e9c3a2f9
locvt1 <locvt1@hdbank.com.vn>
2025-03-21 17:13:37 +0700

    update code

--- benchmark/src/main/java/com/example/lcx/LCXDataSourceBenchmark.java
+++ benchmark/src/main/java/com/example/lcx/LCXDataSourceBenchmark.java
@@ -3,26 +3,22 @@ package com.example.lcx;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Warmup;
 import vn.com.lcx.common.database.pool.LCXDataSource;
 import vn.com.lcx.common.database.pool.entry.ConnectionEntry;
 import vn.com.lcx.common.database.type.DBTypeEnum;
 
-import java.sql.PreparedStatement;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.Throughput)
-@Warmup(iterations = 5, time = 1)
-@Measurement(iterations = 8)
+// @Warmup(iterations = 5, time = 1)
+// @Measurement(iterations = 8)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@State(Scope.Thread)
+@State(Scope.Benchmark)
 public class LCXDataSourceBenchmark {
 
     private LCXDataSource dataSource;
@@ -43,19 +39,17 @@ public class LCXDataSourceBenchmark {
         );
     }
 
-    @TearDown(Level.Trial)
-    public void tearDown() {
-        if (dataSource != null) {
-            // dataSource.close();
-            System.out.println("closed");
-        }
-    }
+    // @TearDown(Level.Trial)
+    // public void tearDown() {
+    //     if (dataSource != null) {
+    //         // dataSource.close();
+    //         System.out.println("closed");
+    //     }
+    // }
 
     @Benchmark
     public void testBorrowAndReturnConnection() throws Exception {
         try (ConnectionEntry conn = dataSource.getConnection()) {
-            // Giả lập thao tác ngắn (không thực hiện truy vấn)
-            System.out.println("borrowed connection");
         }
     }
 
